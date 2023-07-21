3a55d6d5f2ad068a40df464b3283888d79d38f9d
Kavin <kavin.raj@outlook.com>
2023-07-21 09:12:06 -0500

    Initial Commit

---
+++ UniGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniGuide
+//
+//  Created by Kavin Raj on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UniGuide/UniGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniGuideApp.swift
+//  UniGuide
+//
+//  Created by Kavin Raj on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct UniGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
