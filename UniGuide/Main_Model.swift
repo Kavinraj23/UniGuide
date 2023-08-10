d8859ce63445bb6e6aa8892887c33cfd105bb524
Adiv Padgilwar <adivp432@gmail.com>
2023-08-09 21:21:59 -0500

    Model stuffs

---
+++ UniGuide/Main_Model.swift
@@ -0,0 +1,30 @@
+//
+//  Main_Model.swift
+//  UniGuide
+//
+//  Created by Parineeta Padgilwar on 8/9/23.
+//
+
+import Foundation
+
+struct Topic{
+    let id = UUID()
+    let view: [ViewType]
+    let title: String
+    let image: String
+    let sectionsComplete: Int
+}
+
+enum ViewType{
+    case materials(materialInfo: [PrettierInfo])
+    case tips(tipsInfo: [PrettierInfo])
+    case instruction(instructionInfo: [String])
+    case example(exampleInfo: [String])
+}
+        
+struct PrettierInfo{
+    let id = UUID()
+    let text: String
+    let image: String
+    let isBought: Bool?
+}
