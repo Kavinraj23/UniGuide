56acc3ca062618351ab220072c0b182067654af0
Adiv Padgilwar <adivp432@gmail.com>
2023-08-13 19:11:32 -0500

    Model Tweaks and Test Data Input

---
+++ Topic_Data.swift
@@ -0,0 +1,198 @@
+//
+//  Topic_Data.swift
+//  UniGuide
+//
+//  Created by Parineeta Padgilwar on 8/13/23.
+//
+
+import Foundation
+
+//MARK: Test JSON Data
+
+/*
+ 
+ [
+     {
+         "id": "ID_OF_TOPIC",
+         "view": [
+             {
+                 "materials": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Sort clothes before washing. This helps prevent colors from bleeding onto each other and ensures a more effective wash.",
+                             "Use appropriate detergent for different types of fabrics. Using the right detergent can help maintain the quality of your clothes.",
+                             "Check care labels on clothing items before washing. Following care instructions helps prevent damage to delicate fabrics.",
+                             "Separate whites and colors to prevent color transfer during washing. This is especially important for white clothes."
+                         ],
+                         "image": "laundry_image",
+                         "isBought": true
+                     }
+                 ]
+             },
+             {
+                 "tips": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Sort clothes before washing. This helps prevent colors from bleeding onto each other and ensures a more effective wash.",
+                             "Use appropriate detergent for different types of fabrics. Using the right detergent can help maintain the quality of your clothes.",
+                             "Check care labels on clothing items before washing. Following care instructions helps prevent damage to delicate fabrics.",
+                             "Separate whites and colors to prevent color transfer during washing. This is especially important for white clothes."
+                         ],
+                         "image": "laundry_image",
+                         "isBought": true
+                     }
+                 ]
+             },
+             {
+                 "instruction": [
+                     "Sort clothes before washing. This helps prevent colors from bleeding onto each other and ensures a more effective wash.",
+                     "Use appropriate detergent for different types of fabrics. Using the right detergent can help maintain the quality of your clothes.",
+                     "Check care labels on clothing items before washing. Following care instructions helps prevent damage to delicate fabrics.",
+                     "Separate whites and colors to prevent color transfer during washing. This is especially important for white clothes."
+                 ]
+             },
+             {
+                 "example": [
+                     "Separate whites and colors to prevent color transfer during washing.",
+                     "Check care labels on clothing items before washing.",
+                     "Use cold water for delicates.",
+                     "Consider using laundry bags for delicate items."
+                 ]
+             }
+         ],
+         "title": "Laundry Basics",
+         "image": "laundry_image",
+         "sectionsComplete": 2,
+         "quiz": {
+             "questions": [
+                 {
+                     "id": "ID_OF_QUIZ_QUESTION",
+                     "question": "What's a good way to organize laundry?",
+                     "realAnswer": "Sort clothes by color",
+                     "personAnswer": null,
+                     "possibleAnswers": [
+                         "Sort clothes by color",
+                         "Wash everything together",
+                         "Use cold water for everything",
+                         "Sort clothes by fabric type"
+                     ],
+                     "answeredRight": null
+                 }
+             ],
+             "percentage": 0
+         }
+     },
+     {
+         "id": "ID_OF_TOPIC",
+         "view": [
+             {
+                 "materials": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Create a study schedule to allocate time for different subjects and topics. A schedule helps you stay organized and manage your time effectively.",
+                             "Use active learning techniques such as summarization and teaching the material to someone else. Active learning enhances understanding and retention.",
+                             "Summarize readings after each section to reinforce understanding. This practice also makes it easier to review later.",
+                             "Teach the material to someone else. Explaining concepts to someone else helps solidify your understanding."
+                         ],
+                         "image": "study_image",
+                         "isBought": false
+                     }
+                 ]
+             },
+             {
+                 "tips": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Create a study schedule to allocate time for different subjects and topics. A schedule helps you stay organized and manage your time effectively.",
+                             "Use active learning techniques such as summarization and teaching the material to someone else. Active learning enhances understanding and retention.",
+                             "Summarize readings after each section to reinforce understanding. This practice also makes it easier to review later.",
+                             "Teach the material to someone else. Explaining concepts to someone else helps solidify your understanding."
+                         ],
+                         "image": "study_image",
+                         "isBought": false
+                     }
+                 ]
+             },
+             {
+                 "instruction": [
+                     "Create a study schedule to allocate time for different subjects and topics. A schedule helps you stay organized and manage your time effectively.",
+                     "Use active learning techniques such as summarization and teaching the material to someone else. Active learning enhances understanding and retention.",
+                     "Find a quiet study space to minimize distractions.",
+                     "Take breaks during study sessions to stay focused and prevent burnout."
+                 ]
+             },
+             {
+                 "example": [
+                     "Summarize readings after each section to reinforce understanding.",
+                     "Teach the material to someone else to enhance your own comprehension.",
+                     "Use flashcards for memorization of key concepts.",
+                     "Practice solving sample questions to apply your knowledge."
+                 ]
+             }
+         ],
+         "title": "Study Strategies",
+         "image": "study_image",
+         "sectionsComplete": 1,
+         "quiz": {
+             "questions": [
+                 {
+                     "id": "ID_OF_QUIZ_QUESTION",
+                     "question": "How can you better manage your study time?",
+                     "realAnswer": "Create a study schedule",
+                     "personAnswer": null,
+                     "possibleAnswers": [
+                         "Create a study schedule",
+                         "Study only when you feel like it",
+                         "Cram the night before the exam",
+                         "Study for long hours without breaks"
+                     ],
+                     "answeredRight": null
+                 }
+             ],
+             "percentage": 0
+         }
+     },
+     {
+         "id": "ID_OF_TOPIC",
+         "view": [
+             {
+                 "materials": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Introduce yourself to classmates during the first few days of college. Starting conversations early helps establish connections.",
+                             "Attend club meetings and participate in group activities. Clubs provide opportunities to meet people with similar interests.",
+                             "Initiate conversations in common areas like lounges or the cafeteria. These casual interactions can lead to meaningful friendships.",
+                             "Offer to study together with classmates. Studying together is a great way to bond and support each other academically."
+                         ],
+                         "image": "friend_image",
+                         "isBought": true
+                     }
+                 ]
+             },
+             {
+                 "tips": [
+                     {
+                         "id": "ID_OF_PRETTIER_INFO",
+                         "text": [
+                             "Introduce yourself to classmates during the first few days of college. Starting conversations early helps establish connections.",
+                             "Attend club meetings and participate in group activities. Clubs provide opportunities to meet people with similar interests.",
+                             "Initiate conversations in common areas like lounges or the cafeteria. These casual interactions can lead to meaningful friendships.",
+                             "Offer to study together with classmates. Studying together is a great way to bond and support each other academically."
+                         ],
+                         "image": "friend_image",
+                         "isBought": true
+                     }
+                 ]
+             },
+             {
+                 "instruction": [
+                     "Introduce
+
+ 
+ */
+ 
--- UniGuide/Models/Main_Model.swift
+++ UniGuide/Models/Main_Model.swift
@@ -7,37 +7,40 @@
 
 import Foundation
 
-struct Topic{
+struct Topic {
     let id = UUID()
     let view: [ViewType]
     let title: String
     let image: String
     let sectionsComplete: Int
+    let quiz: Quiz?
 }
 
-enum ViewType{
+enum ViewType {
     case materials(materialInfo: [PrettierInfo])
     case tips(tipsInfo: [PrettierInfo])
     case instruction(instructionInfo: [String])
     case example(exampleInfo: [String])
 }
-        
-struct PrettierInfo{
+
+struct PrettierInfo {
     let id = UUID()
-    let text: String
+    let text: [String]
     let image: String
     let isBought: Bool?
 }
 
-struct Quiz{
+struct Quiz {
     let questions: [QuizQuestion]
     var percentage: Int = 0
 }
 
-struct QuizQuestion{
+struct QuizQuestion {
     let id = UUID()
+    let question: String
     let realAnswer: String
     var personAnswer: String?
-    let questions: [String]
+    let possibleAnswers: [String]
     let answeredRight: Bool?
 }
+
